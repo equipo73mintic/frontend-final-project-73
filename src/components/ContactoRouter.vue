47e70412963c88e0094bc9005c9a94dbc8fd3f04
= <gclaros@unicauca.edu.co>
2020-12-24 09:52:15 -0500

    listo testimonios

--- src/components/ContactoRouter.vue
+++ src/components/ContactoRouter.vue
@@ -2,20 +2,35 @@
   <div class="container">
     <div class="card-columns">
       <div class="card">
-        <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCC3KR7RguyaBrHJj1UbGpp4XgqTEwZzyZoQ&usqp=CAU" alt="Card image cap" />
+        <img
+          class="card-img-top"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCC3KR7RguyaBrHJj1UbGpp4XgqTEwZzyZoQ&usqp=CAU"
+          alt="Card image cap"
+        />
         <div class="card-body">
-          <h5 class="card-title">Card title that wraps to a new line</h5>
+          <h5 class="card-title">Daniel Sanchez</h5>
           <p class="card-text">
-            This is a longer card with supporting text below as a natural
-            lead-in to additional content. This content is a little bit longer.
+            Han sido transparentes con nosotros en cada detalle. Acierto total
+            haber instalado placas solares con esta empresa. Han sido
+            transparentes con nosotros en cada detalle de la instalación y se
+            han encargado de todo los trámites de principio a fin, con
+            subvención de la Junta incluida. Han sido muy profesionales y el
+            trato humano inmejorable
           </p>
         </div>
       </div>
       <div class="card p-3">
         <blockquote class="blockquote mb-0 card-body">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-            posuere erat a ante.
+            Por encima de todo destacaría el conocimiento técnico del equipo de
+            esta empresa. Soy ingeniero y contacté con muchas empresas
+            instaladoras de placas solares antes de dar con Quantum. A
+            diferencia de otros comerciales que me visitaron, sus técnicos
+            fueron los únicos que demostraron tener un criterio profundo sobre
+            las soluciones que nos planteaban. Me parece fundamental destacar
+            este punto a los usuarios: cuando te enfrentas a una inversión
+            importante de la que se va a beneficiar toda tu familia quieres
+            estar súper seguro de estar en manos de profesionales.
           </p>
           <footer class="blockquote-footer">
             <small class="text-muted">
@@ -25,12 +40,22 @@
         </blockquote>
       </div>
       <div class="card">
-        <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCC3KR7RguyaBrHJj1UbGpp4XgqTEwZzyZoQ&usqp=CAU" alt="Card image cap" />
+        <img
+          class="card-img-top"
+          src="https://pymstatic.com/44253/conversions/xavier-molina-medium.jpg"
+          alt="Card image cap"
+        />
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
+          <h5 class="card-title">Andres Rojas</h5>
           <p class="card-text">
-            This card has supporting text below as a natural lead-in to
-            additional content.
+            Tras varios años deseando tener autoconsumo con energía limpia en
+            casa terminé optando por GreenEnergy por la calidad y cercanía de su
+            atención al cliente. Después descubrí que el personal técnico es
+            igual de bueno. El presupuesto previo que me enviaron era muy
+            completo y se cumplió todo. En general, si me lees, te recomiendo
+            que te pases al autoconsumo, ya sea de forma individual o colectiva.
+            En Green Energy te ayudan, pero lo importante es que te pases al
+            autoconsumo con renovables lo hagas con quién lo haga
           </p>
           <p class="card-text">
             <small class="text-muted">Last updated 3 mins ago</small>
@@ -40,8 +65,12 @@
       <div class="card bg-primary text-white text-center p-3">
         <blockquote class="blockquote mb-0">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-            posuere erat.
+            Me han realizado una instalación fotovoltaica en una nave y puedo
+            decir que la comunicación la formalidad y el buen trato han sido
+            fantásticos y ademas una grata sorpresa con los instaladores,
+            verdaderos profesionales por su formalidad, organización, limpieza y
+            buen trabajo, hay pocos profesionales así. ¡Chapó!, mi
+            agradecimiento a esta empresa, para mí ejemplar como quedan pocas.
           </p>
           <footer class="blockquote-footer">
             <small>
@@ -52,20 +81,26 @@
       </div>
       <div class="card text-center">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
+          <h5 class="card-title">Sebastian Cobo</h5>
           <p class="card-text">
-            This card has supporting text below as a natural lead-in to
-            additional content.
+            Nos han puesto una instalación fotovoltaica este mes de agosto y
+            todo perfecto. La instalación funciona muy bien, el ahorro en
+            electricidad es grande y ellos son de una profesionalidad
+            difícilmente superable. Muy recomendable.
           </p>
           <p class="card-text">
             <small class="text-muted">Last updated 3 mins ago</small>
           </p>
         </div>
       </div>
-      <div class="card">
-        <img class="card-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCC3KR7RguyaBrHJj1UbGpp4XgqTEwZzyZoQ&usqp=CAU" alt="Card image" />
-      </div>
-      <div class="card p-3 text-right">
+      <!-- <div class="card">
+        <img
+          class="card-img"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCC3KR7RguyaBrHJj1UbGpp4XgqTEwZzyZoQ&usqp=CAU"
+          alt="Card image"
+        />
+      </div> -->
+      <!-- <div class="card p-3 text-right">
         <blockquote class="blockquote mb-0">
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
@@ -77,14 +112,18 @@
             </small>
           </footer>
         </blockquote>
-      </div>
+      </div> -->
       <div class="card">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
+          <h5 class="card-title">Dario Burbano</h5>
           <p class="card-text">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This card has even longer content than the
-            first to show that equal height action.
+            Soy francesa y puedo decir que el nivel de esta empresa es
+            excepcional. Trato muy atento, honesto y transparente. Resolvieron
+            con eficacia todas nuestras dudas sobre cómo podíamos instalar
+            placas en casa. Hoy llevamos seis meses con la instalación y va
+            genial. Estamos gastando la mitad en la factura de la luz. Aunque
+            tardaron un poco en realizarla me alegro mucho y recomiendo colocar
+            placas solares con ellos
           </p>
           <p class="card-text">
             <small class="text-muted">Last updated 3 mins ago</small>
